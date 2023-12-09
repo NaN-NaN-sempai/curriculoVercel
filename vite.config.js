9290df559b2ed40ffe443d66ed8c104883bd6371
Luís Henrique de Almeida <lubizinho-2001@hotmail.com>
2023-12-09 01:54:36 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import ViteCSSExportPlugin from 'vite-plugin-css-export';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), ViteCSSExportPlugin()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
