49645b9248596cb7a9045e5e4018a78251b46e2c
Luís Henrique de Almeida <lubizinho-2001@hotmail.com>
2023-12-09 01:35:05 -0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,14 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+
+		alias: {
+			$style: "src/sass",
+			$assets: "src/assets",
+			$components: "src/components",
+			$fonts: "src/fonts",
+		},
 	}
 };
 
